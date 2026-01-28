96719e8c3f2adb28cf541b4490c791ecff1052e2
janaalturki <163104999+janaalturki@users.noreply.github.com>
2026-01-29 01:45:59 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <head><title>الميدان</title></head>
+      <div>
+        <h1>هلابك في الميدان</h1>
+        <h1>الميداان</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
